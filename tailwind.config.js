8960d3dfbc6f490df279f93212b9e89b552dc736
plum0865 <vivid0865@gmail.com>
2026-03-30 23:42:13 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // 혹시 모르니 추가
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+content: [
+  "app/**/*.{js,ts,jsx,tsx,mdx}",
+  "components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+],
