2be3ef9bb1bc90fbba26effc0432f74924d5f3e4
plum0865 <vivid0865@gmail.com>
2026-03-30 23:45:10 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
-content: [
-  "app/**/*.{js,ts,jsx,tsx,mdx}",
-  "components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // 경로를 최대한 넓고 명확하게 잡습니다.
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./layout.tsx",
+    "./page.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
